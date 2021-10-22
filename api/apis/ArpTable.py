8e24054d11aae303ac11ee327b0470f103c802ae
paul-cruz <poljet12@gmail.com>
2021-10-21 21:34:50 -0500

    Added: arp table model

--- api/apis/ArpTable.py
+++ api/apis/ArpTable.py
@@ -8,65 +8,34 @@ from flask_restplus import Namespace, Resource, fields
 myclient = pymongo.MongoClient(os.getenv("BD_CONN"))
 db = myclient[os.getenv("DB_NAME")]
 propCol = db["properties"]
-api = Namespace('properties', description='properties related operations')
+api = Namespace('arp_table', description='arp table related operations')
 
-apoiment = api.model('Apoiment', {
-    "ref": fields.Integer(),
-    "date": fields.Date()
-})
-
-property = api.model('Properties', {
-    "tittle": fields.String(),
-    "price": fields.Integer(),
-    "features": fields.Raw(example={
-        "land": 0.0,
-        "built": 0.0,
-        "rooms": 0,
-        "bathrooms": 0,
-        "kitchens": 0,
-        "garage": 0
-    }),
-    "address": fields.Raw(example={
-        "line1": "",
-        "state": "",
-        "zip": "",
-        "city": ""
-    }),
-    "location": fields.Raw(example={
-        "lat": 0.0,
-        "long": 0.0
-    }),
-    "description": fields.String(),
-    "type": fields.String(),
-    "age": fields.Integer(),
-    "floors": fields.Integer(),
-    "contact": fields.Raw(example={
-        "name": "",
-        "phones": [""],
-        "email": ""
-    }),
-    "apoiments": fields.List(fields.Nested(apoiment))
+arp_table = api.model('ARP_TABLE', {
+    "interface": fields.String(readonly=True, description='Interface of the device'),
+    "mac": fields.String(),
+    "ip": fields.String(),
+    "age": fields.Float(),
 })
 
 
 @api.route('/')
-@api.response(404, 'Property not inserted')
+@api.response(404, 'arp_table not inserted')
 @api.response(500, 'Server Error')
 class Properties(Resource):
     @api.doc('list_properties')
     def get(self):
         return list(propCol.find())
 
-    @api.doc('post_property')
-    @api.expect(property)
+    @api.doc('post_arp_table')
+    @api.expect(arp_table)
     def post(self):
         try:
             result_id = propCol.insert_one(api.payload).inserted_id
             if result_id:
                 return {'msg': 'Inserted'}, 201
-            raise ValueError('Property not found')
+            raise ValueError('arp_table not found')
         except ValueError as ve:
-            print('Property exception', ve)
+            print('arp_table exception', ve)
             api.abort(404)
         except Exception as e:
             print('Server Error', e)
@@ -74,26 +43,26 @@ class Properties(Resource):
 
 
 @api.route('/<id>')
-@api.param('id', 'The property identifier')
-@api.response(404, 'Property not found')
+@api.param('id', 'The arp_table identifier')
+@api.response(404, 'arp_table not found')
 @api.response(500, 'Server Error')
-class Property(Resource):
-    @api.doc('get_property')
+class arp_table(Resource):
+    @api.doc('get_arp_table')
     def get(self, id):
         try:
             result = propCol.find_one({'_id': ObjectId(id)})
             if result:
                 return result
-            raise ValueError('Property not found')
+            raise ValueError('arp_table not found')
         except ValueError as ve:
-            print('Property exception', ve)
+            print('arp_table exception', ve)
             api.abort(404)
         except Exception as e:
             print('Server Error', e)
             api.abort(500)
 
-    @api.doc('put_property')
-    @api.expect(property)
+    @api.doc('put_arp_table')
+    @api.expect(arp_table)
     def put(self, id):
         try:
             doc = api.payload
@@ -103,23 +72,23 @@ class Property(Resource):
                 return_document=ReturnDocument.AFTER)
             if result:
                 return {'msg': 'Updated'}, 200
-            raise ValueError('Property not found')
+            raise ValueError('arp_table not found')
         except ValueError as ve:
-            print('Property exception', ve)
+            print('arp_table exception', ve)
             api.abort(404)
         except Exception as e:
             print('Server Error', e)
             api.abort(500)
 
-    @api.doc('delete_property')
+    @api.doc('delete_arp_table')
     def delete(self, id):
         try:
             result = propCol.find_one_and_delete({'_id': ObjectId(id)})
             if result:
                 return {'msg': 'Deleted'}, 200
-            raise ValueError('Property not found')
+            raise ValueError('arp_table not found')
         except ValueError as ve:
-            print('Property exception', ve)
+            print('arp_table exception', ve)
             api.abort(404)
         except Exception as e:
             print('Server Error', e)
--- api/apis/__init__.py
+++ api/apis/__init__.py
@@ -1,15 +1,11 @@
 from flask_restplus import Api
-from .Property import api as nsProperties
-from .User import api as nsUsers
-from .Report import api as nsReports
+from .ArpTable import api as nsArpTable
 
 api = Api(
-    title='Trobify API',
+    title='Simple API',
     version='1.0',
-    description='this is the trobify API',
+    description='this is a simple API',
     prefix='/api'
 )
 
-api.add_namespace(nsProperties, path='/property')
-api.add_namespace(nsUsers, path='/user')
-api.add_namespace(nsReports, path='/report')
+api.add_namespace(nsArpTable, path='/arp_table')
